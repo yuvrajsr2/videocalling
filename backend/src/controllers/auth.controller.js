fc713e74fc00dc60635597dd527417c59f122aa3
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-10-21 22:23:51 -0400

    Initialize backend with auth and server setup
    Added initial backend structure including Express server, authentication controller and routes, and configuration files. Includes .gitignore and package files for dependency management.

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req, res){
+    res.send("Singup route");
+}
+
+export async function login(req, res){
+    res.send("Login route");
+}
+
+export function logout(req, res){
+    res.send("Logout route");
+}
---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.route.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+
+
+app.use("/api/auth", authRoutes);
+
+
+
+
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+})

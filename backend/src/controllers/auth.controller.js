260bb09d91ebc372e9da800a97cc0aba3181d110
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-10-27 23:43:02 -0400

    Did the logout route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -100,5 +100,6 @@ export async function login(req, res){
 }
 
 export function logout(req, res){
-    res.send("Logout route");
+    res.clearCookie("jwt");
+    res.status(200).json({success:true, message:"Logged out"});
 }

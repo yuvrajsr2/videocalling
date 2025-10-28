624fc10c7ae38ea61a2667770233f51f40c8f38f
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-10-27 23:47:39 -0400

    Fixed a small typo error in the login route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -79,7 +79,7 @@ export async function login(req, res){
         }
 
 
-        const token = jwt.sign({userId:newUser._id}, process.env.JWT_SECRET, {expiresIn: "7d"},);
+        const token = jwt.sign({userId:user._id}, process.env.JWT_SECRET, {expiresIn: "7d"},);
 
         res.cookie("jwt", token, {
             maxAge: 7 * 24 * 60 * 60 * 1000,

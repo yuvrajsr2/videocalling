42c1fd8b24306fa687536e5e4f198bdaf7724243
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-11-03 22:49:19 -0500

    Changed it again made it beter format

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -25,7 +25,7 @@ const SignUpPage = () => {
             <ShipWheelIcon className='size-9 text-primary' />
 
             <span className='text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary
-            to-secondary tracking-wider'>VIEW</span>
+            to-secondary tracking-wider'>View</span>
 
           </div>
 

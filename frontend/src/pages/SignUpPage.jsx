deb5113a37dc93a169e854ff7f4943e45e57adb6
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-11-03 22:30:34 -0500

    WORKING on signuppage and added lucide react for icons

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {ShipWheelIcon} from 'lucide-react';
 
 const SignUpPage = () => {
+
+  const [signupData, setSignupData] = React.useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+
+  const handleSignup = (e) =>{
+    e.preventDefault();
+    // signup logic here
+  }
   return (
-    <div>
+    <div className='h-screen flex items-center justify-center p-4 sm:p-6 md:p-8' data-theme='forest'>
+      <div className='border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100
+      rounded-xl shadow-lg overflow-hidden'>  
+      
+        <div className='w-full lg:w-1/2 p-4 sm:p-8 flex flex-col '>
+          {/* LOGO */}
+          <div className='mb-4 flex items-center justify-start gap-2'>
+            <ShipWheelIcon className='size-9 text-primary' />
+
+            <span className='text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary
+            to-secondary tracking-wider'>VYOO</span>
+
+          </div>
+        </div>
+      </div>
       
     </div>
   )
